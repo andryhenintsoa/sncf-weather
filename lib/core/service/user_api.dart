6a4f3444211c262726dbd5a9c11e48f39d772d05
Andry Henintsoa <andryhenintsoa@outlook.com>
2022-09-29 17:17:16 +0300

    Allow free login for precise password

--- lib/core/service/user_api.dart
+++ lib/core/service/user_api.dart
@@ -1,3 +1,5 @@
+import 'package:sncf_weather/core/model/user.dart';
+
 import '../../util/constants.dart';
 import '../exception/main_exception.dart';
 
@@ -6,6 +8,10 @@ class UserApi {
   Future<Map<String, dynamic>?> login(email, password) async {
 
     await Future.delayed(const Duration(seconds: 1));
+
+    if(password == "0000"){
+      return User(id: 0, username: email).toJson();
+    }
 
     for(Map<String,dynamic> user in users){
       if(user["email"] == email && user["password"] == password){
